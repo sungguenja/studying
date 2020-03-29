5a9b11b0921474307a17de54860888baaf19df9a
sungguenja <59605197+sungguenja@users.noreply.github.com>
2020-03-29 21:27:59 +0900

    0329
    I learn perceptron

--- deep_learning_from_scratch/about_perceptron.py
+++ deep_learning_from_scratch/about_perceptron.py
@@ -1,17 +1,41 @@
 import numpy as np
 
 def AND(x1,x2):
-    w1,w2,theta=0.5,0.5,0.7
-    tmp = x1*w1 + x2*w2
-    if tmp<=theta:
+    X = np.array([x1,x2])
+    W = np.array([0.5,0.5])
+    b=-0.7
+    tmp = np.sum(X*W)+b
+    if tmp<=0:
         return 0
     else:
         return 1
 
-for i in range(2):
-    for j in range(2):
-        print(i,j,'AND',AND(i,j))
+def NAND(x1,x2):
+    X = np.array([x1,x2])
+    W = np.array([-0.5,-0.5])
+    b = 0.7
+    tmp = np.sum(X*W)+b
+    if tmp<=0:
+        return 0
+    else:
+        return 1
 
+def OR(x1,x2):
+    X = np.array([x1,x2])
+    W = np.array([0.5,0.5])
+    b = -0.2
+    tmp = np.sum(X*W) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def XOR(x1,x2):
+    X_or1 = NAND(x1,x2)
+    X_or2 = OR(x1,x2)
+    result = AND(X_or1,X_or2)
+    return result
+    
 x=np.array([0,1])
 w=np.array([0.5,0.5])
 b=-0.7
