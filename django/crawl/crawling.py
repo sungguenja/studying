7c2f94f52f14891a6257fd3ad8d0f61f20eaddfb
sungguenja <59605197+sungguenja@users.noreply.github.com>
2020-04-08 20:17:48 +0900

    0408
    remind api

--- django/crawl/crawling.py
+++ django/crawl/crawling.py
@@ -3,38 +3,36 @@ import csv
 from bs4 import BeautifulSoup
 
 
-def get_html(url):
-    html=""
-    res = requests.get(url)
+def get_html():
+    adress = 'https://api.themoviedb.org/3/search/movie?api_key='
+    res = requests.get(adress+key+'&query='+'스파이더맨 뉴 유니버스')
+    movie_id = res.json()['results'][0]['id']
+    return movie_res.json().get('posters')
 
-    if res.status_code == 200:
-        html = res.text
+# def crawl(url):
+#     page = []
+
+#     c = get_html(url)
+
+#     soup=BeautifulSoup(c,'html.parser')
+
+#     for tag in soup.select('table[class=wiki-table]'):
+#         if tag.text != '':
+#             page.append(tag.text + '\n')
+#         else:
+#             try:
+#                 img = tag.find('img')
+#                 page.append(img.get('src'))
+#             except AttributeError:
+#                 continue
     
-    return html
+#     return page
 
-def crawl(url):
-    page = []
+# now_data = crawl('')
 
-    c = get_html(url)
+# my_file = open('data4.txt','w', encoding='utf-8')
+# for dt in now_data:
+#     my_file.write(dt + '\n')
 
-    soup=BeautifulSoup(c,'html.parser')
-
-    for tag in soup.select('table[class=wiki-table]'):
-        if tag.text != '':
-            page.append(tag.text + '\n')
-        else:
-            try:
-                img = tag.find('img')
-                page.append(img.get('src'))
-            except AttributeError:
-                continue
-    
-    return page
-
-now_data = crawl('')
-
-my_file = open('data4.txt','w', encoding='utf-8')
-for dt in now_data:
-    my_file.write(dt + '\n')
-
-my_file.close()
+# my_file.close()
+print(get_html())
