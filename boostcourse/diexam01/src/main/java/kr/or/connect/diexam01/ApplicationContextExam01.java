a73a849a67089b815190d4ae0347ab3cfb79a293
sungguenja <59605197+sungguenja@users.noreply.github.com>
2021-01-21 14:26:43 +0900

    intern 4th day studying

---
+++ boostcourse/diexam01/src/main/java/kr/or/connect/diexam01/ApplicationContextExam01.java
@@ -0,0 +1,23 @@
+package kr.or.connect.diexam01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContextExam01 {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		
+		UserBean userBean = (UserBean)ac.getBean("userBean");
+		userBean.setName("kang");
+		
+		System.out.println(userBean.getName());
+		
+		UserBean userBean2 = (UserBean)ac.getBean("userBean");
+		
+		if(userBean == userBean2) {
+			System.out.println("같은인스턴스입니다");
+		}
+	}
+
+}
