ec73c3c1a0de7c64228e33a64ad8cca89a7467a0
sungguenja <sungguenja@gmail.com>
2021-03-18 01:43:15 +0900

    concurrency

---
+++ interview/IntBuffer.java
@@ -0,0 +1,19 @@
+public class IntBuffer {
+    private int index;
+    private int[] buffer = new int[8];
+    public void add( int num ){
+        while ( true ){
+            if ( index < buffer.length ){
+                buffer[index++] = num;
+                return ;
+            }
+        }
+    }
+    public int remove(){
+        while ( true ){
+            if ( index > 0 ){
+                return buffer[--index];
+            }
+        }
+    }
+}
