ccd28860e27fe4692717ac224d9d496d511e6aba
sungguenja <59605197+sungguenja@users.noreply.github.com>
2021-05-24 16:40:14 +0900

    aync promise

---
+++ front cs/async/async.js
@@ -0,0 +1,43 @@
+function delay(ms) {
+    return new Promise(resolve => {setTimeout(resolve,ms)});
+}
+
+async function getApple() {
+    await delay(3000);
+    return 'apple';
+}
+
+async function getBanana() {
+    await delay(3000);
+    return 'banana';
+}
+
+function pickFruits() {
+    return getApple()
+    .then(apple => {
+        return getBanana().then(banana => `${apple} + ${banana}`)
+    })
+}
+
+async function pickFruits2() {
+    const applePromise = getApple();
+    const bananaPromise = getBanana();
+    const apple = await applePromise;
+    const banana = await bananaPromise;
+    return `${apple} + ${banana}`
+}
+
+function pickAllFruits() {
+    return Promise.all([getApple(),getBanana()]).then((fruits) => {
+        return fruits.join(' + ')
+    })
+}
+
+pickFruits().then((result) => {
+    console.log(result)
+})
+
+getApple()
+.then((result) => {console.log(result)})
+
+pickAllFruits().then((result) => {console.log(result)})
---
+++ front cs/async/promise.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const promise = new Promise((resolve,reject) => {
+    /* 
+    doing some heaby work ()
+    resolve는 성공했을시 어떤 값을 전달해줄지
+    reject는 실패했을시 어떤 값을 전달해줄지이다
+    */
+    console.log('doing something');
+    resolve("ellie");
+    reject(new Error('no network'))
+});
+
+promise.then((value) => {
+    console.log(value);
+})
+.catch((error) => {
+    console.log(error)
+})
+.finally(() => {
+    console.log('finally')
+})
+
+const fetch_number = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve(1)
+    },1000);
+})
+
+fetch_number
+.then(num => num*2)
+.then(num => num*3)
+.then(num => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => resolve(num-1))
+    },1000);
+})
+.then(num => console.log(num));
